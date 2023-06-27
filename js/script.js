438234b80d6c0521c3d6d23c2e481c95483086e0
ThainaZanfolin <thaina.zanfolin@gmail.com>
2023-06-26 22:36:31 -0300

    iniciando js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//seleções
+
+const todoForm = document.querySelector("#to-do-form");
+const todoInput = document.querySelector("#to-do-input");
+const todosList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+
+//funções
+
+//eventos
